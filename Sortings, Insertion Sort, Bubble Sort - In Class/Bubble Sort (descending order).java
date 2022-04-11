92821bfcec7c8a767339b4eeb4be501a7062f3ad
Dipak Taur <dipaktaur92@gmail.com>
2022-04-11 17:00:17 +0530

    Create Bubble Sort (descending order).java

---
+++ Sortings, Insertion Sort, Bubble Sort - In Class/Bubble Sort (descending order).java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Main{
+    public static void BubbleSort(int arr[]) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] < arr[j + 1]) {
+                    int D = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = D;
+                }
+            }
+        }
+     for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+  public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+        int arr[] = new int[n];
+
+		for(int i=0;i<n;i++){
+			arr[i]=sc.nextInt();
+		}
+        BubbleSort(arr);
+    
+    }
+}
