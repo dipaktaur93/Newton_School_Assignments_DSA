50f8b8a0d35de11e97984983e3e104d7a27c738b
Dipak Taur <dipaktaur92@gmail.com>
2022-04-11 15:05:34 +0530

    Create Donation.java

---
+++ 1-D Array, Maths - In Class/Donation.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String[] args){
+    Donation();
+    }
+    public static void Donation(){
+        Scanner sc = new Scanner(System.in);
+        int numOfPeople = sc.nextInt();
+        long[] perHeadDonation = new long[numOfPeople];
+        long sum=0;
+        for(int i = 0; i<numOfPeople;i++){
+            perHeadDonation[i]=sc.nextInt();
+            sum=sum+perHeadDonation[i];
+        }
+        System.out.print ("0 ");//for first donation
+        for(int i = 1;i<perHeadDonation.length;i++){
+
+            if(perHeadDonation[i]<perHeadDonation[i-1]){
+                System.out.print (perHeadDonation[i-1]-perHeadDonation[i]+" ");
+                sum=sum+(perHeadDonation[i-1]-perHeadDonation[i]);
+                perHeadDonation[i]=perHeadDonation[i-1];
+            }
+            else{
+             System.out.print("0 ");
+            }
+        }
+        System.out.println("");
+        System.out.print(sum)   ;
+    }
+}
