1291ef9d51032f9b5c5e73a2df3e0e4156b6440e
Dipak Taur <dipaktaur92@gmail.com>
2022-04-11 15:03:06 +0530

    Create DoraemonAndBigLight(challenge).java

---
+++ Java/JDK Intro & Setup, Variables & Operators, Input, Output and Exceptions, Con  - Post Class/DoraemonAndBigLight(challenge).java
@@ -0,0 +1,12 @@
+static boolean EqualOrNot(int h1, int h2, int v1,int v2){
+//Enter your code here
+if (v1 < v2){}
+while (h1 >=h2){
+  if (h1 ==h2){
+    return true;
+  }
+  h1= h1+v1;
+  h2= h2+v2;
+}
+return false;
+}
