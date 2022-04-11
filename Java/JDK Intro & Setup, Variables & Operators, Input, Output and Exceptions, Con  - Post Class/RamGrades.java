c04119cf63a2eb42119c973ee0cb203a0a27ebc9
Dipak Taur <dipaktaur92@gmail.com>
2022-04-11 15:01:46 +0530

    Create RamGrades.java

---
+++ Java/JDK Intro & Setup, Variables & Operators, Input, Output and Exceptions, Con  - Post Class/RamGrades.java
@@ -0,0 +1,18 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+	public static void main (String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    int c = sc.nextInt();
+    int d = sc.nextInt();
+    int sum = a+b+c+d;
+    int percentage = sum/4;
+    System.out.println(percentage);
+    sc.close();
+	}
+}
