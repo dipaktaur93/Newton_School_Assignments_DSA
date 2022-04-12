c7b3d4b2769cff076be48deae964ffb8f85c7540
Dipak Taur <dipaktaur92@gmail.com>
2022-04-12 10:29:19 +0530

    Create Print the Linked List.java

---
+++ Single Linked List - In Class/Print the Linked List.java
@@ -0,0 +1,17 @@
+/*
+class Node {
+    Node next;
+    int val;
+    Node(int val) {
+        this.val = val;
+        next = null;
+    }
+}
+// */
+     public static void printList(Node head) {
+        Node currNode = head;
+        while (currNode != null) {
+            System.out.print(currNode.val + " ");
+            currNode = currNode.next;
+        }
+    }
