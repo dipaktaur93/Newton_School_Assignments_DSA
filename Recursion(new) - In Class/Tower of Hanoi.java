c32126e7b89c90292ad3f32fa2ec8903cb27496f
Dipak Taur <dipaktaur92@gmail.com>
2022-08-03 10:28:43 +0530

    Create Tower of Hanoi.java

---
+++ Recursion(new) - In Class/Tower of Hanoi.java
@@ -0,0 +1,26 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+	  static void towerOfHanoi(int n, char from_rod, char to_rod, char aux_rod){
+            if (n == 0)
+            {
+                return;
+            }
+            towerOfHanoi(n - 1, from_rod, aux_rod, to_rod);
+            System.out.println(n+":"+from_rod+"->"+to_rod);
+            towerOfHanoi(n - 1, aux_rod, to_rod, from_rod);
+        }
+
+        public static void  main(String args[])
+        {
+            int n;
+            Scanner sc=new Scanner(System.in);
+            n=sc.nextInt();
+            towerOfHanoi(n, 'A', 'C', 'B');
+        }
+
+
+}
